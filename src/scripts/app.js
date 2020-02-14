58c97985f11d6abeef7475dff9b196b722664e5a
Firzinat Khuzeev <firzinat.khuzeev@akvelon.com>
2020-02-14 11:46:28 +0300

    refact(styles): add css grid

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,18 @@
-console.log('Test');
+console.log("Test");
+
+(function() {
+  const mediumViewButton = document.getElementById("medium-view");
+  mediumViewButton.addEventListener("click", function() {
+    console.log("mediumViewButton clicked");
+  });
+
+  const smallViewButton = document.getElementById("small-view");
+  smallViewButton.addEventListener("click", function() {
+    console.log("smalliewButton clicked");
+  });
+
+  const listViewButton = document.getElementById("list-view");
+  listViewButton.addEventListener("click", function() {
+    console.log("listViewButton clicked");
+  });
+})();
