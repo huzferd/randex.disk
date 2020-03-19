de4d47e6dd00df30db57a81c30567c181eae3365
Firzinat Khuzeev <v-fikhuz@microsoft.com>
2020-03-19 11:55:54 +0300

    refact(styles): update header styles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,37 @@
-console.log("Test");
-
 (function() {
   const mediumViewButton = document.getElementById("large-view");
   let setLargeView = function() {
-    document.querySelectorAll(".content-browser").forEach(el => el.className = "content-browser content-browser_large");
-    document.querySelectorAll(".content-item").forEach(el => el.className = "content-item content-item_large");
-    document.querySelectorAll(".content-item__column_date").forEach(el => el.className = "content-item__column_date content-item__column_hidden");
-    document.querySelectorAll(".content-item__column_time").forEach(el => el.className = "content-item__column_time content-item__column_hidden");
-    document.querySelectorAll(".content-item__column_size").forEach(el => el.className = "content-item__column_size content-item__column_hidden");
-    document.querySelectorAll(".content-item__icon").forEach(el => el.className = "content-item__icon");
-  }
+    document
+      .querySelectorAll(".content-browser")
+      .forEach(el => (el.className = "content-browser content-browser_large"));
+    document
+      .querySelectorAll(".content-item")
+      .forEach(el => (el.className = "content-item content-item_large"));
+    document
+      .querySelectorAll(".content-item__column_date")
+      .forEach(
+        el =>
+          (el.className =
+            "content-item__column_date content-item__column_hidden")
+      );
+    document
+      .querySelectorAll(".content-item__column_time")
+      .forEach(
+        el =>
+          (el.className =
+            "content-item__column_time content-item__column_hidden")
+      );
+    document
+      .querySelectorAll(".content-item__column_size")
+      .forEach(
+        el =>
+          (el.className =
+            "content-item__column_size content-item__column_hidden")
+      );
+    document
+      .querySelectorAll(".content-item__icon")
+      .forEach(el => (el.className = "content-item__icon"));
+  };
 
   setLargeView();
 
@@ -17,21 +39,68 @@ console.log("Test");
 
   const smallViewButton = document.getElementById("small-view");
   smallViewButton.addEventListener("click", function() {
-    document.querySelectorAll(".content-browser").forEach(el => el.className = "content-browser content-browser_small");
-    document.querySelectorAll(".content-item").forEach(el => el.className = "content-item content-item_small");
-    document.querySelectorAll(".content-item__column_date").forEach(el => el.className = "content-item__column_date content-item__column_hidden");
-    document.querySelectorAll(".content-item__column_time").forEach(el => el.className = "content-item__column_time content-item__column_hidden");
-    document.querySelectorAll(".content-item__column_size").forEach(el => el.className = "content-item__column_size content-item__column_hidden");
-    document.querySelectorAll(".content-item__icon").forEach(el => el.className = "content-item__icon");
+    document
+      .querySelectorAll(".content-browser")
+      .forEach(el => (el.className = "content-browser content-browser_small"));
+    document
+      .querySelectorAll(".content-item")
+      .forEach(el => (el.className = "content-item content-item_small"));
+    document
+      .querySelectorAll(".content-item__column_date")
+      .forEach(
+        el =>
+          (el.className =
+            "content-item__column_date content-item__column_hidden")
+      );
+    document
+      .querySelectorAll(".content-item__column_time")
+      .forEach(
+        el =>
+          (el.className =
+            "content-item__column_time content-item__column_hidden")
+      );
+    document
+      .querySelectorAll(".content-item__column_size")
+      .forEach(
+        el =>
+          (el.className =
+            "content-item__column_size content-item__column_hidden")
+      );
+    document
+      .querySelectorAll(".content-item__icon")
+      .forEach(el => (el.className = "content-item__icon"));
   });
 
   const listViewButton = document.getElementById("list-view");
   listViewButton.addEventListener("click", function() {
-    document.querySelectorAll(".content-browser").forEach(el => el.className = "content-browser content-browser_list");
-    document.querySelectorAll(".content-item").forEach(el => el.className = "content-item content-item_list");
-    document.querySelectorAll(".content-item__column_date").forEach(el => el.className = "content-item__column_date");
-    document.querySelectorAll(".content-item__column_time").forEach(el => el.className = "content-item__column_time");
-    document.querySelectorAll(".content-item__column_size").forEach(el => el.className = "content-item__column_size");
-    document.querySelectorAll(".content-item__icon").forEach(el => el.className = "content-item__icon content-item__icon_list");
+    document
+      .querySelectorAll(".content-browser")
+      .forEach(el => (el.className = "content-browser content-browser_list"));
+    document
+      .querySelectorAll(".content-item")
+      .forEach(el => (el.className = "content-item content-item_list"));
+    document
+      .querySelectorAll(".content-item__column_date")
+      .forEach(el => (el.className = "content-item__column_date"));
+    document
+      .querySelectorAll(".content-item__column_time")
+      .forEach(el => (el.className = "content-item__column_time"));
+    document
+      .querySelectorAll(".content-item__column_size")
+      .forEach(el => (el.className = "content-item__column_size"));
+    document
+      .querySelectorAll(".content-item__icon")
+      .forEach(
+        el => (el.className = "content-item__icon content-item__icon_list")
+      );
+  });
+
+  window.addEventListener("scroll", e => {
+    const el = document.getElementById("header");
+    if (window.scrollY > 0) {
+      el.classList.add("header_scroll");
+    } else {
+      el.classList.remove("header_scroll");
+    }
   });
 })();
